b201e37cb7da2b886106adb7f25adfbc96443153
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-01-16 00:42:39 +0530

    search first part done

--- frontened/src/components/Body.js
+++ frontened/src/components/Body.js
@@ -1,19 +1,77 @@
+import React from "react";
 import Carousel from "./Carousel.js";
 import Resturent from "./ResturentCard.js";
 import { ResturentList } from "../confic.js";
-import React from "react";
+import { useState } from "react";
+
+// Filter Function
+function fliterResto(searchTxt, Resto) {
+  const filterDatas = Resto.filter((resto) => {
+    return resto?.data?.name?.toLowerCase()?.includes(searchTxt.toLowerCase());
+  });
+  return filterDatas;
+}
 
 const Body = () => {
+  // Declaring The state for the searchtext
+  const [searchTxt, setSearchTxt] = useState("");
+  //Declaring the state for the resto list so that we can pass the list for seacrh resto in the map
+  const [resturentList, setResturentList] = useState(ResturentList);
+
   return (
     <>
       <Carousel />
 
       {/**
+       * Search Area
+       */}
+      <div className="form-control mt-5   ">
+        <div className="input-group w-full j sm:justify-center md:justify-end">
+          <input
+            type="text"
+            placeholder="Search…"
+            className="input input-bordered"
+            value={searchTxt}
+            onChange={(e) => {
+              setSearchTxt(e.target.value);
+            }}
+          />
+          <button
+            className="btn btn-square"
+            onClick={() => {
+              // A function will be call to filter the resto for search
+              const filterData = fliterResto(searchTxt, resturentList);
+              // setSearchTxt(filterData);
+              console.log("filter Data", filterData);
+              setResturentList(filterData);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      {/**
        * This the Card Components
        */}
       <div className=" mt-5 w-4/5 mx-auto container  space-y-4 item-start justify-between resturen-list f flex flex-row flex-wrap space-x-1 ">
-        {ResturentList.map((resto, index) => {
-          return <Resturent {...resto.data} key={index} />;
+        {resturentList.map((resto) => {
+          console.log("helllo");
+          console.log(resto);
+          return <Resturent {...resto.data} key={resto.data.id} />;
         })}
       </div>
     </>
--- frontened/src/components/ResturentCard.js
+++ frontened/src/components/ResturentCard.js
@@ -1,5 +1,12 @@
 import { IMG_URL } from "../confic.js";
-const Resturent = (name, cuisines, city, cloudinaryImageId, avgRating) => {
+const Resturent = ({
+  name,
+  cuisines,
+  city,
+  slugs,
+  cloudinaryImageId,
+  avgRating,
+}) => {
   return (
     <>
       <div className="card w-56 hover:scale-110 transform transition duration-500   border bg-base-100 shadow-xl ">
@@ -8,10 +15,10 @@ const Resturent = (name, cuisines, city, cloudinaryImageId, avgRating) => {
         </figure>
         <div className="card-body p-1">
           <h2 className="card-title">{name}</h2>
-          <p>{cuisines.join("'")}</p>
+          <p>{cuisines}</p>
           <div className="card-actions ">
-            <div className="badge badge-outline">{city}</div>
-            <div className="badge badge-outline">{avgRating}</div>
+            <div className="badge badge-outline">{slugs.city}</div>
+            <div className="badge badge-outline">{avgRating}start</div>
           </div>
         </div>
       </div>
