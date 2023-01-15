e1388db7b6ac6ade84eb3b6b34882faf913c5c4c
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-01-15 22:49:39 +0530

    Implement the dynamic card

--- frontened/src/components/Body.js
+++ frontened/src/components/Body.js
@@ -1,5 +1,7 @@
 import Carousel from "./Carousel.js";
 import Resturent from "./ResturentCard.js";
+import { ResturentList } from "../confic.js";
+import React from "react";
 
 const Body = () => {
   return (
@@ -10,11 +12,9 @@ const Body = () => {
        * This the Card Components
        */}
       <div className=" mt-5 w-4/5 mx-auto container  space-y-4 item-start justify-between resturen-list f flex flex-row flex-wrap space-x-1 ">
-        {Array(10)
-          .fill("")
-          .map((resto, index) => {
-            return <Resturent key={index} />;
-          })}
+        {ResturentList.map((resto, index) => {
+          return <Resturent {...resto.data} key={index} />;
+        })}
       </div>
     </>
   );
--- frontened/src/components/ResturentCard.js
+++ frontened/src/components/ResturentCard.js
@@ -1,16 +1,17 @@
-const Resturent = () => {
+import { IMG_URL } from "../confic.js";
+const Resturent = (name, cuisines, city, cloudinaryImageId, avgRating) => {
   return (
     <>
       <div className="card w-56 hover:scale-110 transform transition duration-500   border bg-base-100 shadow-xl ">
         <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          <img src={IMG_URL + cloudinaryImageId} alt="Shoes" />
         </figure>
         <div className="card-body p-1">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title">{name}</h2>
+          <p>{cuisines.join("'")}</p>
           <div className="card-actions ">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+            <div className="badge badge-outline">{city}</div>
+            <div className="badge badge-outline">{avgRating}</div>
           </div>
         </div>
       </div>
